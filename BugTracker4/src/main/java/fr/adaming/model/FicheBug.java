49eaba65530fa06e8f7149db475553e148d17e1f
Purpleproject <intijuin2017@gmail.com>
2017-08-30 14:08:36 +0200

    14:08 ajout fichebug

---
+++ BugTracker4/src/main/java/fr/adaming/model/FicheBug.java
@@ -0,0 +1,150 @@
+package fr.adaming.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="fichebugs")
+public class FicheBug  implements Serializable{
+	//attributs
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id_fichebug")
+	private int id;
+		
+	private String libelle;
+	private String description;
+	private String statut;
+	
+	//association many to one
+	@ManyToOne
+	@JoinColumn(name="testeur_id",referencedColumnName="id_utilisateur")
+	private Testeur testeur;
+	//association many to one
+	@ManyToOne
+	@JoinColumn(name="test_id",referencedColumnName="id_test")
+	private Test test;
+	
+
+	/*******************************************
+	*				Constructeurs
+	********************************************/
+	
+	/**
+	 * Constructeur vide
+	 */
+	public FicheBug() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * Constructeur avec id
+	 * @param id
+	 * @param testeur
+	 * @param test
+	 * @param libelle
+	 * @param description
+	 * @param statut
+	 */
+	public FicheBug(int id, Testeur testeur, Test test, String libelle, String description, String statut) {
+		super();
+		this.id = id;
+		this.testeur = testeur;
+		this.test = test;
+		this.libelle = libelle;
+		this.description = description;
+		this.statut = statut;
+	}
+
+	/**
+	 * Constructeur sans id
+	 * @param testeur
+	 * @param test
+	 * @param libelle
+	 * @param description
+	 * @param statut
+	 */
+	public FicheBug(Testeur testeur, Test test, String libelle, String description, String statut) {
+		super();
+		this.testeur = testeur;
+		this.test = test;
+		this.libelle = libelle;
+		this.description = description;
+		this.statut = statut;
+	}  
+	
+	/*************************************
+	 * 		Asseseurs et mutateurs 
+	 * @return
+	 ************************************/
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Testeur getTesteur() {
+		return testeur;
+	}
+
+	public void setTesteur(Testeur testeur) {
+		this.testeur = testeur;
+	}
+
+	public Test getTest() {
+		return test;
+	}
+
+	public void setTest(Test test) {
+		this.test = test;
+	}
+
+	public String getLibelle() {
+		return libelle;
+	}
+
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getStatut() {
+		return statut;
+	}
+
+	public void setStatut(String statut) {
+		this.statut = statut;
+	}
+
+	/*************************
+	 * toString
+	 **************************/
+	@Override
+	public String toString() {
+		return "FicheBug [id=" + id + ", test=" + test + ", libelle=" + libelle + ", description=" + description
+				+ ", statut=" + statut + "]";
+	}
+	
+	
+	 
+	
+
+}
