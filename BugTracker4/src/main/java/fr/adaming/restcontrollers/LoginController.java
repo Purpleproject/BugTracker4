33c2a84e6cff7f75fe3b8b6099d66df236d826c2
Purpleproject <intijuin2017@gmail.com>
2017-08-31 14:25:48 +0200

    14:25 Guillaume: loginController 2ème

--- BugTracker4/src/main/java/fr/adaming/restcontrollers/LoginController.java
+++ BugTracker4/src/main/java/fr/adaming/restcontrollers/LoginController.java
@@ -13,15 +13,7 @@ public class LoginController {
 	
 	@RequestMapping(value="/login", method=RequestMethod.GET)
 	public void login(){
-		String req="SELECT count(1) FROM Utilisateur WHERE mail=? AND mdp=?";
 		
-		if (req>0)
-			u 
-		else
 			null;
-		//if mail + mdp correspond à un élément de la BD
-		//envoyer sur la page d'accueil du site
-		//else
-		//renvoyer vers la page login + un message d'erreur
 	}
 }
