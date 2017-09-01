0437016c63f893c512b6dc1e9c4335735af33693
Purpleproject <inti.juin2017@gmail.com>
2017-09-01 13:06:38 +0200

    13.06 marieaure utilisateurctrl

--- BugTracker4/src/main/java/fr/adaming/restcontrollers/UtilisateurController.java
+++ BugTracker4/src/main/java/fr/adaming/restcontrollers/UtilisateurController.java
@@ -1,8 +1,6 @@
-package fr.adaming.restcontrollers; 
+package fr.adaming.restcontrollers;
 
 import java.util.List;
-
-
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -22,7 +20,7 @@ import fr.adaming.service.IUtilisateurService;
 @RestController
 @RequestMapping("/user")
 public class UtilisateurController {
-//
+	//
 	@Autowired
 	IUtilisateurService utilisateurSer;
 
@@ -45,19 +43,19 @@ public class UtilisateurController {
 	}
 
 	@CrossOrigin
-	@RequestMapping(value = "/creerTesteur", method = RequestMethod.POST,consumes="application/json",produces="application/json")
+	@RequestMapping(value = "/creerTesteur", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
 	public void creer(@RequestBody Testeur user) {
 		testeurSer.creer(user);
 		System.out.println("ok");
 	}
 
 	@CrossOrigin
-	@RequestMapping(value = "/creerEditeur", method = RequestMethod.POST,consumes="application/json",produces="application/json")
+	@RequestMapping(value = "/creerEditeur", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
 	public void creer(@RequestBody Editeur user) {
 		editeurSer.creer(user);
 		System.out.println("ok");
 	}
-	
+
 	@CrossOrigin
 	@RequestMapping(value = "/rechercherTout", method = RequestMethod.GET, produces = "application/json")
 	public List<Utilisateur> rechercherTout() {
@@ -69,28 +67,29 @@ public class UtilisateurController {
 	public List<Testeur> rechercherToutTesteurs() {
 		return testeurSer.rechercheTout();
 	}
-	
-@CrossOrigin
-@RequestMapping(value = "/editeurId", method = RequestMethod.GET, produces = "application/json")
-public Editeur rechercherEditeurId(@RequestParam("pId")int id) {
-return editeurSer.rechercheParId(id);
-}
-	
-@CrossOrigin
-@RequestMapping(value = "/testeurId", method = RequestMethod.GET, produces = "application/json")
-public Testeur rechercherTesteurId(@RequestParam("pId")int id) {
- return testeurSer.rechercheParId(id);
-}
-}
 
-//	@RequestMapping(value = "/authentification", method = RequestMethod.GET, produces = "application/json")
-//	public boolean authentification(@RequestParam("pMail") String mail, @RequestParam("pMdp") String mdp) {
-//		Utilisateur user = utilisateurSer.rechercheParMail(mail);
-//		if (user != null & user.getMpd() == mdp) {
-//			return true;
-//		} else {
-//			return false;
-//		}
-//	}
+	@CrossOrigin
+	@RequestMapping(value = "/editeurId", method = RequestMethod.GET, produces = "application/json")
+	public Editeur rechercherEditeurId(@RequestParam("pId") int id) {
+		return editeurSer.rechercheParId(id);
+	}
+
+	@CrossOrigin
+	@RequestMapping(value = "/testeurId", method = RequestMethod.GET, produces = "application/json")
+	public Testeur rechercherTesteurId(@RequestParam("pId") int id) {
+		return testeurSer.rechercheParId(id);
+	}
+
+	// @RequestMapping(value = "/authentification", method = RequestMethod.GET,
+	// produces = "application/json")
+	// public boolean authentification(@RequestParam("pMail") String mail,
+	// @RequestParam("pMdp") String mdp) {
+	// Utilisateur user = utilisateurSer.rechercheParMail(mail);
+	// if (user != null & user.getMpd() == mdp) {
+	// return true;
+	// } else {
+	// return false;
+	// }
+	// }
 
 }
